c844e27dc6c8f940d8d6dcfcc40a9117a520a995
Sathish R <sathishrachapalli7716@gmail.com>
2024-11-16 22:52:10 +0530

    adding the project

---
+++ src/main/java/com/example/configurations/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.example"})
+public class AppConfig implements WebMvcConfigurer {
+	
+
+
+}
---
+++ src/main/java/com/example/smControllers/studentController.java
@@ -0,0 +1,22 @@
+package com.example.smControllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class studentController {
+	
+	@GetMapping("/show")
+	@ResponseBody
+	public String testing(Model model)
+	{
+	
+		return "testing done successfully!!!";
+	}
+	
+	
+		
+
+}
